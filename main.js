f85194f5f1e96c073650c3e6e80b832b20619595
Jacq Babb <jacq@jeuq.me>
2020-10-20 20:31:12 -0400

    recent work update

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
-// eslint-disable-next-line strict
-function main() {
-    $('#number-chooser').on('submit', function(evt){
-        evt.preventDefault(); 
-        console.log(evt)
-        let number = $('#number-choice').val(); // get value from text box
-        console.log(number);
-        for (let i = 0; i<=number; i++) {
-        
-            if (i%3===0 && i%5===0) {
-                console.log('fizzbuzz');
-            }
 
-            else if(i%3===0){
-                console.log('fizz');
-            }
-            
-            else if(i%5===0) {
-                console.log('buzz');
-            }
 
-            else {
-                console.log(i);
-            }
-        }        
+$main () {
+    $('#number-choice').on('submit,'  function (fb(evt) {
+      const input = $(evt.number-choice);
+      console.log(input);
     });
-}
+  }
+
+/*when submit is clicked
+  take number entered
+  check if divisible by 3 and 5
+  check if divisible by 3
+  check if divisible by 5
+  otherwise print number
+  assign styles to the above
+  console.log .js-results
+
+*/
 
 $(main);
